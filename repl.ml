74a430d04ef8b01bc469f02be1326cce363c3f27
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-01-22 22:32:07 +0100

    Implement booleans, if-expression and some test functions

--- repl.ml
+++ repl.ml
@@ -13,6 +13,7 @@ type 'b sexp = Value of 'b | Sexp of 'b sexp list
 type ltype =
   | LUnit
   | LInt of int
+  | LBool of bool
   | LString of string
   | LCons of ltype * ltype
   | LIdentifier of string
@@ -32,6 +33,7 @@ let rec string_of_ltype =
   function
   | LUnit -> "()"
   | LInt(i) -> string_of_int i
+  | LBool(b) -> if b then "#t" else "#f"
   | LString(s) -> s
   | LCons(c, d) as x -> "(" ^ string_of_lcons x ^ ")"
   | LIdentifier(s) -> s
@@ -50,6 +52,14 @@ let int_of_ltype = function
   | LInt(i) -> i
   | v -> raise (CannotCast (string_of_ltype v, "int"))
 
+let is_bool (x: string) =
+  x = "#t" || x = "#f"
+
+let bool_of_ltype_string b =
+  if b = "#t" then true
+  else if b = "#f" then false
+  else raise (CannotCast (b, "bool"))
+
 let is_string (x: string) =
   let length = String.length x in
   (x.[0] = '\'' && x.[length - 1] = '\'')
@@ -64,6 +74,7 @@ let rec ltype_of_sexp (expression: string sexp): ltype =
   match expression with
   | Value(v) ->
       if is_int v then LInt(int_of_string v)
+      else if is_bool v then LBool(bool_of_ltype_string v)
       else if is_string v then LString(v)
       else if is_identifier v then LIdentifier(v)
       else raise (Failure "Cannot convert sexp to ltype!")
@@ -117,6 +128,10 @@ let rec add_variables_to_context ctxt vars vals: context =
   | _ -> raise TypeError
 
 let rec eval (ctxt: context) (expression: ltype): ltype =
+  let validate_if = function
+    | LCons(LIdentifier("if"),
+        LCons(predicate, LCons(then_branch, LCons(else_branch, LUnit)))) -> ()
+    | x -> raise (IllFormedSpecialForm (string_of_ltype x)) in
   let validate_lambda = function
     | LCons(LIdentifier("lambda"), LCons(lambda_args, LCons(lambda_body, LUnit)))
       when ltype_is_list lambda_args -> ()
@@ -127,11 +142,22 @@ let rec eval (ctxt: context) (expression: ltype): ltype =
   match expression with
   | LUnit -> LUnit
   | LInt(_) as x -> x
+  | LBool(_) as x -> x
   | LString(_) as x -> x
   | LIdentifier(v) -> begin match lookup ctxt v with
     | Some value  -> value
     | None -> raise (UnboundValue v)
   end
+  | LCons(LIdentifier("if"), _) as if_expression ->
+    validate_if if_expression;
+    let predicate = ltype_car (ltype_cdr if_expression) in
+    let then_branch = ltype_car (ltype_cdr (ltype_cdr if_expression)) in
+    let else_branch = ltype_car (ltype_cdr (ltype_cdr (ltype_cdr if_expression))) in
+    begin match (eval ctxt predicate) with
+      | LBool(true) -> eval ctxt then_branch
+      | LBool(false) -> eval ctxt else_branch
+      | _ -> raise TypeError
+    end
   | LCons(LIdentifier("lambda"), _) as lambda ->
     validate_lambda lambda;
     let lambda_args = ltype_car (ltype_cdr lambda) in
@@ -217,29 +243,55 @@ let rec parse_input (source: unit -> string): string sexp =
       raise NotSexp in
   parse_sexp (read_word (trim (source ())))
 
-let check_arguments expected actual =
+let bad_arguments expected actual =
   raise (ArgumentsMismatch(expected, actual))
 
 let global_context: context = ref
   [("+", LFunction(fun arguments ctxt ->
-     LInt(int_of_ltype(ltype_car arguments) + int_of_ltype(ltype_car (ltype_cdr arguments)))));
+     let length = ltype_length arguments in
+     if length = 2 then
+       let f = ltype_car arguments in
+       let s = ltype_car (ltype_cdr arguments) in
+       LInt(int_of_ltype f + int_of_ltype s)
+     else bad_arguments 2 length));
+   ("-", LFunction(fun arguments ctxt ->
+     let length = ltype_length arguments in
+     if length = 2 then
+       let f = ltype_car arguments in
+       let s = ltype_car (ltype_cdr arguments) in
+       LInt(int_of_ltype f - int_of_ltype s)
+     else bad_arguments 2 length));
+   ("*", LFunction(fun arguments ctxt ->
+     let length = ltype_length arguments in
+     if length = 2 then
+       let f = ltype_car arguments in
+       let s = ltype_car (ltype_cdr arguments) in
+       LInt(int_of_ltype f * int_of_ltype s)
+     else bad_arguments 2 length));
+   ("=", LFunction(fun arguments ctxt ->
+     let length = ltype_length arguments in
+     if length = 2 then
+       let f = ltype_car arguments in
+       let s = ltype_car (ltype_cdr arguments) in
+       LBool(int_of_ltype f = int_of_ltype s)
+     else bad_arguments 2 length));
    ("cons", LFunction(fun arguments ctxt ->
      let length = ltype_length arguments in
      if length = 2 then
        let f = ltype_car arguments in
        let s = ltype_car (ltype_cdr arguments) in
        LCons(f, s)
-     else check_arguments 2 length));
+     else bad_arguments 2 length));
    ("car", LFunction(fun arguments ctxt ->
      let length = ltype_length arguments in
      if length = 1 then
        ltype_car (ltype_car arguments)
-     else check_arguments 1 length));
+     else bad_arguments 1 length));
    ("cdr", LFunction(fun arguments ctxt ->
      let length = ltype_length arguments in
      if length = 1 then
        ltype_cdr (ltype_car arguments)
-     else check_arguments 1 length))]
+     else bad_arguments 1 length))]
 
 let rec repl () = begin
   let error str = print_endline ("Error: " ^ str) in
@@ -276,9 +328,9 @@ let rec repl () = begin
           | CannotCast(value, to_type) ->
               error ("Cannot cast " ^ value ^ " to type " ^ to_type ^ "!")
           | UnboundValue(s) -> error ("Unbound value: " ^ s)
-          | ArgumentsMismatch(expected, got) -> error ("Wrong number of
-          arguments provided! Expected " ^ string_of_int expected ^ " but got "
-          ^ string_of_int got ^ "!")
+          | ArgumentsMismatch(expected, got) -> error ("Wrong number of "
+          ^ "arguments provided: Expected " ^ string_of_int expected
+          ^ " but got " ^ string_of_int got ^ "!")
           | TypeError -> error ("The object is not of the correct type")
           | IllFormedSpecialForm(text) -> error ("Ill-formed special form: " ^ text)
   end;
@@ -290,6 +342,10 @@ let dummy_sexp = (Sexp([Value("cons"); Value("1"); Value("2")]))
 let test_ltype = string_of_ltype (LCons(LInt(1), LCons(LInt(2), LCons(LInt(3), LUnit))))
 let test_sexp = eval global_context(ltype_of_sexp dummy_sexp)
 let test_cons = string_of_ltype test_sexp
+let dummy_define_lambda = "(define fact (lambda (x) (if (= x 0) 1 (* x (fact (- x 1))))))"
+let dummy_define_lambda_sexp = parse_input (fun () -> dummy_define_lambda)
+let dummy_define_lambda_tokens = ltype_of_sexp dummy_define_lambda_sexp
+let test_define_lambda = eval global_context dummy_define_lambda_tokens
 
 let _ =
   repl ()
