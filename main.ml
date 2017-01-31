f07848a0e0aa1d3bdd940203995c60af8c7f2927
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-02-01 00:42:11 +0100

    Add some command line arguments to main program

--- main.ml
+++ main.ml
@@ -1,3 +1,28 @@
+let file_before_repl: string ref = ref ""
+let verbose: bool ref = ref false
+
+let command_line_arguments = [
+  ("-f", Arg.Set_string(file_before_repl), "Load specified file before starting repl");
+  ("-v", Arg.Set(verbose), "Execute specified files verbosely");
+]
+
+let usage = "Usage: " ^ Sys.argv.(0) ^ " [-f filename] [-v] [filename]"
+
 let _ =
   let _ = Repl.load_stdlib () in
-  Repl.repl ()
+  let file_to_execute: string ref = ref "" in
+  let _ = Arg.parse command_line_arguments
+    (fun filename ->
+      let _ = print_endline filename in
+      file_to_execute := filename)
+    usage in
+    if String.length !file_before_repl != 0 then
+      let _ = Repl.load_file !file_before_repl (not !verbose) in
+      ()
+    else
+      ();
+    if String.length !file_to_execute != 0 then
+      let _ = Repl.load_file !file_to_execute (not !verbose) in
+      ()
+    else
+      Repl.repl ()
